47eaf641312ce406beed758fa718a122ccca324b
Munirah Abdulkarim <abdulkarimmunirah6@gmail.com>
2025-03-22 06:51:03 +0100

    Added a new react app, styled it and used state hook

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "gray",
+        color: "darkgray",
+        padding: "10px",
+        fontSize: "30px",
+      }}
+    >
+      <p style={{ color: "blue", textAlign: "center" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
