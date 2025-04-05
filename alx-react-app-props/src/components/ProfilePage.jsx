847d3e0023054c56913b9d19d1d3ee7af84e2419
Munirah Abdulkarim <abdulkarimmunirah6@gmail.com>
2025-04-05 15:19:53 +0100

    Completed the refactoring od Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,15 @@
+// import UserInfo from "./UserInfo";
+
+// function ProfilePage({ userData }) {
+//   return <UserInfo userData={userData} />;
+// }
+
+// export default ProfilePage;
+
+import UserInfo from "./UserInfo";
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,26 @@
+// function UserDetails({ userData }) {
+//   return (
+//     <div>
+//       <p>Name: {userData.name}</p>
+//       <p>Email: {userData.email}</p>
+//     </div>
+//   );
+// }
+
+// export default UserDetails;
+
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
